b49bad129c09d14e8bdafa1671db18cade9bcbe0
pdewan <dewan@cs.unc.edu>
2015-10-22 09:49:27 -0400

    Printing on console view. Changed build.properties, which was inconsistent with plugin.xml

--- src/unc/cs/checks/UNCCheck.java
+++ src/unc/cs/checks/UNCCheck.java
@@ -3,6 +3,11 @@ package unc.cs.checks;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.eclipse.ui.console.ConsolePlugin;
+import org.eclipse.ui.console.IConsole;
+import org.eclipse.ui.console.IConsoleManager;
+import org.eclipse.ui.console.MessageConsole;
+
 import com.puppycrawl.tools.checkstyle.api.Check;
 import com.puppycrawl.tools.checkstyle.api.DetailAST;
 import com.puppycrawl.tools.checkstyle.api.FileContents;
@@ -13,10 +18,52 @@ public abstract class UNCCheck extends Check{
 	protected String checkAndFileDescription = "";
 	protected static boolean errorOccurred;
 	public static final String ERROR_KEY = "checkStyleError";
+	public static final String CONSOLE_NAME = "UNCChecks";
+	protected MessageConsole console;
+	protected boolean notInPlugIn;
+	  protected MessageConsole findConsole() {
+		  if (notInPlugIn)
+			  return null;
+		  try {
+		  if (console == null)
+			  console = findConsole(CONSOLE_NAME);
+		  return console;
+		  } catch (Exception e) {
+			  return null;
+		  }
+	  }
+	  protected void consoleOut(String aString) {
+		  if (notInPlugIn)
+			  return;
+		  console.newMessageStream().println(aString);
+	  }
+	  protected MessageConsole findConsole(String name) {
+		  try {
+			  // see if we are executing in a plug in
+			  Class.forName("org.eclipse.ui.console.ConsolePlugin");
+		  } catch (Exception e){
+			  notInPlugIn = true;
+			  return null;
+		  }
+	      ConsolePlugin plugin = ConsolePlugin.getDefault();
+	      IConsoleManager conMan = plugin.getConsoleManager();
+	      IConsole[] existing = conMan.getConsoles();
+	      for (int i = 0; i < existing.length; i++)
+	         if (name.equals(existing[i].getName()))
+	            return (MessageConsole) existing[i];
+	      //no console found, so create a new one
+	      MessageConsole myConsole = new MessageConsole(name, null);
+	      conMan.addConsoles(new IConsole[]{myConsole});
+	      return myConsole;
+	   }
 
 	public final void extendibleLog(int line, String key, Object... args) {
 //		System.out.println("key:" + key);
+		try {
         log(line, key, args);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
     }
 	
 	public abstract void doVisitToken(DetailAST ast);
@@ -52,7 +99,9 @@ public abstract class UNCCheck extends Check{
 		} catch (RuntimeException e) {
 			e.printStackTrace();
 			errorOccurred = true;
+
 			log(ast, ERROR_KEY, "Begin tree:" + checkAndFileDescription + " " + e.getMessage());
+			consoleOut(e.getMessage() + " Description:" + checkAndFileDescription + "Stack:\n" + toString(e.getStackTrace()));
 
 //			throw e;
 			
@@ -60,12 +109,21 @@ public abstract class UNCCheck extends Check{
     	
     }
 
+    public static String toString (StackTraceElement[] aStackTrace ) {
+    	StringBuffer result = new StringBuffer();
+    	for (StackTraceElement anElement:aStackTrace) {
+    		result.append(anElement.toString() + "\n");
+    	}
+    	return result.toString();
+    }
 
 	public void finishTree(DetailAST ast) {
 		if (errorOccurred)
     		return;
 		try {
-			System.out.println ("finish tree called from:" + this + " ast:" + ast + " " + getFileContents().getFilename());
+//			System.out.println ("finish tree called from:" + this + " ast:" + ast + " " + getFileContents().getFilename());
+//			findConsole().newMessageStream().println("finish tree called from:" + this + " ast:" + ast + " " + getFileContents().getFilename());
+
 //			if (ast.getType() == TokenTypes.LITERAL_NEW) {
 //				System.out.println ("found new");
 //			}
@@ -79,9 +137,11 @@ public abstract class UNCCheck extends Check{
 			
 		} catch (RuntimeException e) {
 			System.out.println("Description:" + checkAndFileDescription);
+
 			e.printStackTrace();
 			errorOccurred = true;
-			log(ast, ERROR_KEY, "Finish tree:" + checkAndFileDescription + " " + e.getMessage());
+			log(ast, ERROR_KEY, "Finish tree:" + checkAndFileDescription + " " + e.getStackTrace());
+			consoleOut(e.getMessage() + " Description:" + checkAndFileDescription + "Stack:\n" + toString(e.getStackTrace()));
 
 //			throw e;
 			
@@ -106,7 +166,10 @@ public abstract class UNCCheck extends Check{
 		} catch (RuntimeException e) {
 			e.printStackTrace();
 			errorOccurred = true;
+
+//			findConsole().newMessageStream().println("Stack:" + e.getStackTrace());
 			log(ast, ERROR_KEY, "Visit token:" + checkAndFileDescription + " " + e.getMessage());
+			consoleOut(e.getMessage() + " Description:" + checkAndFileDescription + "Stack:\n" + toString(e.getStackTrace()));
 
 
 //			throw e;
