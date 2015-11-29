36a1ed0bd22eb6874af33ded0ed42c45bf54ad2b
pdewan <dewan@cs.unc.edu>
2015-11-28 23:10:08 -0500

    ClassIsGeneric

---
+++ src/unc/cs/checks/ClassIsGenericCheck.java
@@ -0,0 +1,64 @@
+package unc.cs.checks;
+
+import java.util.List;
+
+import unc.cs.symbolTable.STNameable;
+import unc.cs.symbolTable.STType;
+import unc.cs.symbolTable.SymbolTableFactory;
+
+import com.puppycrawl.tools.checkstyle.api.Check;
+import com.puppycrawl.tools.checkstyle.api.TokenTypes;
+import com.puppycrawl.tools.checkstyle.api.DetailAST;
+import com.sun.org.apache.bcel.internal.util.Class2HTML;
+
+
+public  class ClassIsGenericCheck extends STClassVisitedComprehensively {
+	/**
+	 * A key is pointing to the warning message text in "messages.properties"
+	 * file.
+	 */
+	public static final String MSG_KEY = "classIsGeneric";
+
+//	/** flag to control whether marker interfaces are allowed. */
+//	private boolean allowMarkerInterfaces = true;
+	
+	public  ClassIsGenericCheck () {
+		
+	}
+//	@Override
+//	public int[] getDefaultTokens() {
+//		return new int[] { TokenTypes.CLASS_DEF };
+//	}
+//	@Override
+//    public void visitClass(DetailAST typeDef) { 
+//		super.visitClass(typeDef);
+//    	
+//		STType anSTClass = SymbolTableFactory.getOrCreateSymbolTable().
+//    			getSTClassByFullName(fullTypeName);
+//    	if (!typeCheck(anSTClass))
+//    		log(typeDef);
+//
+//    }
+//	
+//	protected void log(DetailAST ast) {
+//	    log(getNameAST(ast).getLineNo(), msgKey(), fullTypeName);
+//    }
+
+
+	protected Boolean typeCheck(STType anSTClass) {
+		DetailAST generic = typeAST.findFirstToken(TokenTypes.TYPE_PARAMETERS);
+				return generic != null;
+	}
+
+//	protected void log(DetailAST ast) {
+//		log(ast.getLineNo(), msgKey(), typeName);
+//	}
+	@Override
+	protected String msgKey() {
+		// TODO Auto-generated method stub
+		return MSG_KEY;
+	}
+
+	
+	
+}
--- src/unc/cs/checks/TypeVisitedCheck.java
+++ src/unc/cs/checks/TypeVisitedCheck.java
@@ -40,7 +40,12 @@ public abstract class TypeVisitedCheck extends UNCCheck {
 		}
 		return false;
 	}
-	 public void visitType(DetailAST ast) {  
+	 public void visitType(DetailAST ast) { 
+//		 DetailAST generic = ast.getFirstChild().getNextSibling().getNextSibling().getNextSibling();
+//		 generic = ast.findFirstToken(TokenTypes.TYPE_PARAMETERS);
+//		 if (generic.getType() == TokenTypes.TYPE_PARAMETERS) {
+//			 int i = 0;
+//		 }
 //	    	
 //			maybeVisitPackage(ast);
 	    	typeAST = ast;
