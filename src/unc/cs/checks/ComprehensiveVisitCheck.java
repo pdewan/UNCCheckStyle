53613ad1a5b82961728c9e202899b108b273e3cb
pdewan <dewan@cs.unc.edu>
2018-12-06 10:40:09 -0500

    Checking for null pointer exception in isIdentifier

--- src/unc/cs/checks/ComprehensiveVisitCheck.java
+++ src/unc/cs/checks/ComprehensiveVisitCheck.java
@@ -179,7 +179,7 @@ public abstract class ComprehensiveVisitCheck extends TagBasedCheck implements
 	}
 
 	public static Boolean isIdentifier(String aString) {
-		if (aString.length() == 0)
+		if (aString == null || aString.length() == 0)
 			return false;
 		char aFirstChar = aString.charAt(0);
 		if (!Character.isLetter(aFirstChar) && (aFirstChar != TAG_CHAR)) {
